082205f0ef699553f6fae3f2651cb6ad577a1316
Allison Richardet <allison@tetriscodes.com>
2018-03-29 22:22:31 -0500

    remove Command & Event types - now in proto

--- model/db.go
+++ model/db.go
@@ -1,27 +1,5 @@
 package model
 
-import (
-	"time"
-
-	"github.com/google/uuid"
-)
-
-type Command struct {
-	ID        uuid.UUID              `json:"id,omitempty"`
-	Action    string                 `json:"action"`
-	Data      map[string]interface{} `json:"data"`
-	Timestamp time.Time              `json:"timestamp"`
-	Children  uuid.UUID              `json:"children_id"`
-}
-
-type Event struct {
-	ID        uuid.UUID              `json:"id"`
-	Action    string                 `json:"action"`
-	Data      map[string]interface{} `json:"data"`
-	Timestamp time.Time              `json:"timestamp"`
-	Parent    uuid.UUID              `json:"parent_id"`
-}
-
 type DB struct {
 }
 
