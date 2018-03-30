30e432d959eed273fca7feac127131b9392a52a9
Allison Richardet <allison@tetriscodes.com>
2018-03-30 09:38:19 -0500

    move create event & command to model/db, return error

--- api/api.go
+++ api/api.go
@@ -1,20 +1,7 @@
 package api
 
 import (
-	"cloud.google.com/go/datastore"
-	"github.com/pkg/errors"
 	"github.com/tetriscode/commander/model"
-	"github.com/tetriscode/commander/proto"
-)
-
-type KindType string
-
-const (
-	// KindTypeCommand for datastore
-	KindTypeCommand KindType = "Command"
-
-	// KindTypeEvent for datastore
-	KindTypeEvent KindType = "Event"
 )
 
 func (q *Queue) CreateCommand(cmd *model.Command) {
@@ -23,23 +10,4 @@ func (q *Queue) CreateCommand(cmd *model.Command) {
 
 func (q *Queue) CreateCommandSync(cmd *model.Command) {
 
-}
-
-// CreateCommand creates a Command entity and stores it in datastore
-func CreateCommand(command proto.Command) {
-	entity := command
-	k = datastore.IDKey(KindTypeCommand, command.Id, nil)
-	k, err = dsClient.Put(ctx, k, e)
-	if err != nil {
-		errors.Wrapf(err, "failed to put %s %s to datastore", KindTypeCommand, command.Id)
-	}
-}
-
-// CreateEvent creates en Event entity and stores it in datastore
-func CreateEvent(event proto.Event) {
-	k = datastore.IDKey(KindTypeEvent, event.Id, nil)
-	k, err = dsClient.Put(ctx, k, e)
-	if err != nil {
-		errors.Wrapf(err, "failed to put %s %s to datastore", KindTypeEvent, event.Id)
-	}
 }
--- model/db.go
+++ model/db.go
@@ -19,3 +19,34 @@ func (db *DB) Start() error {
 func (db *DB) Stop(err error) {
 
 }
+
+type KindType string
+
+const (
+	// KindTypeCommand for datastore
+	KindTypeCommand KindType = "Command"
+
+	// KindTypeEvent for datastore
+	KindTypeEvent KindType = "Event"
+)
+
+// CreateCommand creates a Command entity and stores it in datastore
+func CreateCommand(command proto.Command) return {
+	entity := command
+	k = datastore.IDKey(KindTypeCommand, command.Id, nil)
+	k, err = dsClient.Put(ctx, k, e)
+	if err != nil {
+		return errors.Wrapf(err, "failed to put %s %s to datastore", KindTypeCommand, command.Id)
+	}
+	return nil
+}
+
+// CreateEvent creates en Event entity and stores it in datastore
+func CreateEvent(event proto.Event) return {
+	k = datastore.IDKey(KindTypeEvent, event.Id, nil)
+	k, err = dsClient.Put(ctx, k, e)
+	if err != nil {
+		return errors.Wrapf(err, "failed to put %s %s to datastore", KindTypeEvent, event.Id)
+	}
+	return nil
+}
